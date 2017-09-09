0118c6eea41b3ace4337de7b7bcf655aa9e3b402
Laurent Ros <laurentr@theodo.fr>
2017-09-09 14:08:10 +0200

    [Webpack] Create src alias to PinPic

---
+++ preact.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+/**
+ * Function that mutates original webpack config.
+ * Supports asynchronous changes when promise is returned.
+ *
+ * @param {object} config - original webpack config.
+ * @param {object} env - options passed to CLI.
+ * @param {WebpackConfigHelpers} helpers - object with useful helpers when working with config.
+ **/
+export default function (config) {
+  config.resolve.alias.PinPic = path.resolve('./src');
+}
