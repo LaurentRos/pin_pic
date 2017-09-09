234f8586568f9b82cb2a9dba956db892ea6fdf45
Laurent Ros <laurentr@theodo.fr>
2017-09-09 14:53:25 +0200

    [Maps] Display Maps component and display it

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,14 @@
 import { Component } from 'preact';
 
 import Header from './header';
+import MapContainer from './maps';
 
 export default class App extends Component {
     render() {
         return (
             <div id="app">
                 <Header />
+                <MapContainer />
             </div>
         );
     }
